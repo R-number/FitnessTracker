df4b18fb0538feac6230f3b71538a5c724500ff7
Marcus Corbin <mgc1g18@soton.ac.uk>
2021-02-24 21:05:21 +0000

    BT communication code added

---
+++ BTComms.cpp
@@ -0,0 +1,46 @@
+/**
+* Authour:      Team R-Number
+* Date Created: 24/02/21
+* File name:    BTComms.cpp
+*/
+
+#include "BTComms.h"
+
+#define STATUS_PIN  27
+#define EN_PIN      28
+
+BTComms::BTComms(HardwareSerial &BTSerialPort)
+    :m_BT(BTSerialPort)
+
+{
+    /* empty constructor */
+}
+
+void BTComms::init()
+{
+    m_BT.begin(9600);            // begin comms with HC-05 module, default baud rate is 9600
+    pinMode(STATUS_PIN, INPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(EN_PIN, OUTPUT);
+}
+
+void BTComms::status()
+{
+    digitalWrite(LED_BUILTIN, digitalRead(STATUS_PIN));  // set the led based on the HC-05 current status
+}
+
+void BTComms::read()
+{
+    if(m_BT.available())
+    {
+        Serial.write(m_BT.read());  // read whatever is incoming from BT and write to debug serial port
+    }
+}
+
+void BTComms::write()
+{
+        if(Serial.available())
+    {
+        m_BT.write(Serial.read());  // read whatever is coming from Serial and write to BT
+    }
+}
--- FitnessTracker.ino
+++ FitnessTracker.ino
@@ -1,20 +1,27 @@
 /**
 * Authour:      Team R-Number
 * Date Created: 24/02/21
+* File name:    FitnessTracker.ino
 */
 
-#include "rtc.h"
+#include "DSRtc.h"
+#include "BTComms.h"
 
 DSRtc rtc;
+BTComms btComms(Serial1);       // setup the bluetooth with Serial port 1
 
 void setup()
 {
     Serial.begin(115200);       // begin serial for debug
-    rtc.init();                 // init the rtc object
+    rtc.init();                 // init the rtc
+    btComms.init();             // init the bt comms
 }
 
 void loop()
 {
     rtc.printTime();            // print out the current time
+    
+    btComms.read();
+    btComms.write();
 
 }
