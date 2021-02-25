90d3dca8dbe4527d4124cc852698cd56fe6ebacf
Marcus Corbin <mgc1g18@soton.ac.uk>
2021-02-25 11:58:39 +0000

    Display + analogue read called

--- FitnessTracker.ino
+++ FitnessTracker.ino
@@ -6,22 +6,30 @@
 
 #include "DSRtc.h"
 #include "BTComms.h"
+#include "Display.h"
 
 DSRtc rtc;
 BTComms btComms(Serial1);       // setup the bluetooth with Serial port 1
+Display display;
+
+#define HEARTRATE_PIN   A0
+
 
 void setup()
 {
-    Serial.begin(115200);       // begin serial for debug
-    rtc.init();                 // init the rtc
-    btComms.init();             // init the bt comms
+    Serial.begin(115200);               // begin serial for debug - this is used in other init functions to give current status
+    rtc.init();                         // init the rtc
+    btComms.init();                     // init the bt comms
+    display.init();                     // init the display - note this also runs multiple test functions
 }
 
 void loop()
 {
-    rtc.printTime();            // print out the current time
+    rtc.printTime();                    // print out the current time
     
     btComms.read();
     btComms.write();
 
+    Serial.print("Heartrate val: ");    // read heartrate and print to console
+    Serial.println(analogRead(HEARTRATE_PIN));
 }
