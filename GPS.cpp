d070477b79598a52cb25d48824a8afd719cfc016
Marcus Corbin <mgc1g18@soton.ac.uk>
2021-03-02 19:06:53 +0000

    Can now calculate distance travelled (total)

--- GPS.cpp
+++ GPS.cpp
@@ -7,6 +7,7 @@ File name:      GPS.cpp
 #include "GPS.h"
 
 #define GPS_PORT_NAME   "Serial2"
+#define DEBUG_SERIAL
 
 GPS::GPS(HardwareSerial &GPSSerialPort)
     :m_gpsPort(GPSSerialPort)
@@ -56,20 +57,18 @@ void GPS::init()
                         "  You should confirm this with NMEAorder.ino\n") );
                         
     m_gpsPort.begin(9600);
+
+    m_dist = 0;
+    m_locValid = false;
 }
 
-void GPS::monitor()
+void GPS::loop()
 {
-
-    // while(m_gpsPort.available() > 0)
-    // {
-    //     Serial.write(m_gpsPort.read());
-    // }
-
-    while(m_gpsParser.available(m_gpsPort))
+    while(m_gpsParser.available(m_gpsPort))     // check if we have data incoming
     {
-        m_fix = m_gpsParser.read();
+        m_fix = m_gpsParser.read();             // read in the latest fix
 
+        #ifdef DEBUG_SERIAL
         Serial.print("Location: ");
         if (m_fix.valid.location) 
         {
@@ -84,6 +83,79 @@ void GPS::monitor()
             Serial.print( m_fix.altitude() );
         }
 
-        Serial.println();
+        Serial.println();   
+        #endif
+
+        calculateDistance();                // calculate the distance when we get a new fix
+    }
+}
+
+
+void GPS::calculateDistance()
+{
+    static uint16_t count = 0;
+    static int32_t latAccumulator = 0, lonAccumulator = 0;
+    const uint16_t n_samples = 15;
+
+    /* 
+    * this function samples valid points to create an average postion
+    * this is required as otherwise we will accumulate distance when
+    * static due to gps jitter
+    */
+
+    if(m_fix.valid.location && m_fix.valid.date && m_fix.valid.time)        // do we have a valid fix?
+    {
+        latAccumulator += m_fix.location.lat();
+        lonAccumulator += m_fix.location.lon();
+        count ++;
+
+        if(count >= n_samples)                  // when we have completed sampling
+        {
+            #ifdef DEBUG_SERIAL
+            Serial.print("Accum(lat,long): ");
+            Serial.print(latAccumulator);
+            Serial.print(", ");
+            Serial.println(lonAccumulator);
+            #endif
+
+            latAccumulator /= n_samples;        // average the data
+            lonAccumulator /= n_samples;
+            m_currentLoc.lat(latAccumulator);   // load averaged data into current location
+            m_currentLoc.lon(lonAccumulator);
+            latAccumulator = 0;                 // reset for next time
+            lonAccumulator = 0;
+
+            #ifdef DEBUG_SERIAL
+            Serial.print("Avg(lat,long): ");
+            Serial.print(m_currentLoc.lat());
+            Serial.print(", ");
+            Serial.println(m_currentLoc.lon());
+            Serial.print("Dist(delta): ");
+            Serial.print(m_currentLoc.DistanceKm(m_oldLoc), 10);
+            #endif
+
+            /* only accumulate the distance when location is valid and we are above certain speed */
+            if(m_locValid && (m_fix.speed_kph() > 0.5f))          
+            {
+                m_dist += m_currentLoc.DistanceKm(m_oldLoc);    // calculate distance between old and new
+
+                #ifdef DEBUG_SERIAL
+                Serial.print("Dist(km): ");
+                Serial.println(m_dist, 6);
+                #endif
+            }
+            m_oldLoc = m_currentLoc;                            // old = new
+            count = 0;
+            m_locValid = true;                                  // we have been round once (or more) - location is valid
+        }
+    }
+    else
+    {
+        #ifdef DEBUG_SERIAL
+        Serial.println("No GPS");
+        #endif
+
+        m_locValid = false;
+        count = 0;
     }
 }
--- GPS.h
+++ GPS.h
@@ -15,10 +15,22 @@ public:
     GPS(HardwareSerial &GPSSerialPort);
 
     void init();
-    void monitor();
+    void loop();
+    bool isValid() const { return m_locValid; }
+
+    float getAltitude() const { return m_fix.latitude(); }
+    float getLongitude() const { return m_fix.longitude(); }
+    float getLatitude() const { return m_fix.latitude(); }
+    float getDistance() const { return m_dist; }
+
+private:
+    void calculateDistance();
 
 private:
     HardwareSerial &m_gpsPort;
     NMEAGPS m_gpsParser;
     gps_fix m_fix;
+    NeoGPS::Location_t m_currentLoc, m_oldLoc;      // used to calculate distance travelled
+    float m_dist;
+    bool m_locValid;
 };
