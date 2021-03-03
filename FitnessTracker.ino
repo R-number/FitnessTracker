89956c4c81a52ccd1c5f4278c5e2d5ccbce6015a
JayantKalyan2001 <jk5g19@soton.ac.uk>
2021-03-03 13:28:44 +0000

    Added Heart rate  functionality (needs testing)

--- FitnessTracker.ino
+++ FitnessTracker.ino
@@ -7,6 +7,7 @@
 #include "DSRtc.h"
 #include "BTComms.h"
 #include "Display.h"
+#include "DFRobot_Heartrate.h"
 
 DSRtc rtc;
 #if defined(ARDUINO_SAM_DUE)
@@ -15,28 +16,30 @@ BTComms btComms(Serial1);       // setup the bluetooth with Serial port 1 for Ar
 BTComms btComms(Serial);        // for other arduino's use the only serial port
 #endif
 Display display;
-
+DFRobot_Heartrate heartSense(DIGITAL_MODE);
 #define HEARTRATE_PIN   A0
 
 
 void setup()
 {
-    Serial.begin(115200);               // begin serial for debug - this is used in other init functions to give current status
-    rtc.init();                         // init the rtc
+    Serial.begin(9600);               // begin serial for debug - this is used in other init functions to give current status
+    //rtc.init();                         // init the rtc
     btComms.init();                     // init the bt comms
-    display.init();                     // init the display - note this also runs multiple test functions
+   // display.init();                     // init the display - note this also runs multiple test functions
 }
 
 void loop()
 {
-    DateTime now = rtc.get();
-    display.showTime(now);
-
+    //DateTime now = rtc.get();
+    heartSense.getValue(HEARTRATE_PIN);
+    //uint16_t val = analogRead(HEARTRATE_PIN);
+    uint16_t bpmValue = heartSense.getRate();
+   // Serial.print("Analogue: ");
+    //Serial.println(val);
+    Serial.print("BPM: ");
+    Serial.println(bpmValue);
+   // display.showTime(now);
     delay(500);
-    btComms.read();
-    btComms.write();
-    // display.increment();
-
-    // Serial.print("Heartrate val: ");    // read heartrate and print to console
-    // Serial.println(analogRead(HEARTRATE_PIN));
+   btComms.read();
+   btComms.write();
 }
