7ef2f1a08e0cd9a063bb871f45531d29a2ab4a0f
JayantKalyan2001 <jk5g19@soton.ac.uk>
2021-03-05 16:57:40 +0000

    Non Blocking BPM working (some redundant debugging variables remain)

--- FitnessTracker.ino
+++ FitnessTracker.ino
@@ -18,10 +18,10 @@ BTComms btComms(Serial);        // for other arduino's use the only serial port
 Display display;
 DFRobot_Heartrate heartSense(DIGITAL_MODE);
 #define HEARTRATE_PIN   A0
-#define BUFFER_SAMPLES   200
+#define BUFFER_SAMPLES   500
 uint8_t bpmValue = 0;
 static uint32_t deltaTime = 0;
-uint8_t rateBuffer=0, idle=0 ,ThrownRate=0;
+uint16_t rateBuffer=0, idle=0 ,ThrownRate=0, SucessRate=0;          //Remove Thrown Rate once the system is working
 
 uint16_t cumulativeRate=0;    
 uint8_t bpm =0;
@@ -39,42 +39,68 @@ uint8_t StabiliseRate()
         
             if (bpm)
             {
-                //Serial.print(rateBuffer);
-                //Serial.print(" BPM: ");
-               // Serial.println(bpm);
+                SucessRate++;
                 //Serial.print(" Delta time: ");
-                //Serial.println(deltaTime);
+                //Serial.print(deltaTime);
+                Serial.print(" Number of Measurements: ");
+                Serial.print(rateBuffer);
+                Serial.print(" ,Number of Idles: ");
+                Serial.print(idle);
+                Serial.print(" ,Number of Thrown  Measurements: ");
+                Serial.print(ThrownRate);
+                Serial.print(" ,Number of Successful  Measurements: ");
+                Serial.print(SucessRate);
+                Serial.print(" ,Number of Cumulative  Measurements: ");
+                Serial.print(cumulativeRate);
+                Serial.print(" ,BPM: ");
+                Serial.println(bpm);
+
                 idle=0;
+           
                 cumulativeRate+=bpm;    
                        
             }
             else
             {
-               // Serial.print(rateBuffer);
-                //Serial.print(" BPM: ");
-               // Serial.println(bpm);
-               // Serial.print(" Delta time: ");
-               // Serial.println(deltaTime);
+                //Serial.print(" Delta time: ");
+                //Serial.print(deltaTime);
+                Serial.print(" Number of Measurements: ");
+                Serial.print(rateBuffer);
+                Serial.print(" ,Number of Idles: ");
+                Serial.print(idle);
+                Serial.print(" ,Number of Thrown  Measurements: ");
+                Serial.print(ThrownRate);
+                Serial.print(" ,Number of Successful  Measurements: ");
+                Serial.print(SucessRate);
+                Serial.print(" ,Number of Cumulative  Measurements: ");
+                Serial.print(cumulativeRate);
+                Serial.print(" ,BPM: ");
+                Serial.println(bpm);
+
                 idle++;
                 ThrownRate++;
             }             
             rateBuffer++; 
         } 
     }
-    
-    if(rateBuffer==200)
-    {
-        rateBuffer=0;
-        idle=0;
-        ThrownRate=0;
-    }
     else
     {
         Serial.println("Waiting for delay");
     }
-    if (idle<80)
+    
+    if(rateBuffer==BUFFER_SAMPLES)
     {
-        return cumulativeRate/(BUFFER_SAMPLES-ThrownRate);
+        Serial.println("Buffer reset");
+        rateBuffer=0;
+        idle=0;
+        ThrownRate=0;
+        SucessRate=0;
+        cumulativeRate=0;
+    }
+
+    if (idle<0.05*BUFFER_SAMPLES)
+    {
+        return cumulativeRate/SucessRate;
     }
     else
         return 0;           
@@ -84,7 +110,7 @@ void setup()
 {
     Serial.begin(9600);               // begin serial for debug - this is used in other init functions to give current status
     //rtc.init();                         // init the rtc
-    btComms.init();                     // init the bt comms
+    //btComms.init();                     // init the bt comms
    // display.init();                     // init the display - note this also runs multiple test functions
 }
 
