9fb0df375a2a3d6d9c87371808d57c5c646adb20
Marcus Corbin <mgc1g18@soton.ac.uk>
2021-03-07 20:29:59 +0000

    BTcomms stuff

--- FitnessTracker.ino
+++ FitnessTracker.ino
@@ -12,16 +12,12 @@
 #include "HeartRate.h"
 
 DSRtc rtc;
-#if defined(ARDUINO_SAM_DUE)
-BTComms btComms(Serial1);       // setup the bluetooth with Serial port 1 for Arduino Due
 GPS gps(Serial2);
-#else
-BTComms btComms(Serial);        // for other arduino's use the only serial port
-GPS gps(Serial);
-#endif
 Display display;
 StepCount stepCount;
 HeartRate heartRate;
+
+BTComms btComms(Serial1, rtc, gps, stepCount, heartRate);
 
 
 void setup()
@@ -62,6 +58,6 @@ void loop()
     heartRate.loop();
 
     btComms.read();
-    btComms.write();
+    // btComms.write();
 }
 
