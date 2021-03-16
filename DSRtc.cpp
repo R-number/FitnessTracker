03c84507e3efa44a9a8fb5e97f710690c8a1ca23
Marcus Corbin <mgc1g18@soton.ac.uk>
2021-03-16 17:23:03 +0000

    comments added

--- DSRtc.cpp
+++ DSRtc.cpp
@@ -22,26 +22,27 @@ void DSRtc::init()
     }
     Serial.println("init RTC");
 
-    /* debug only (would request this from bt connected phone) */
-    m_rtc.adjust(DateTime());    // set date time to compile date & time
-    // print(get());
+    /* will be changed when bt connects to phone) */
+    m_rtc.adjust(DateTime());    // set date time to defualt dt
 }
 
-DateTime DSRtc::get() const
+DateTime DSRtc::get() const     // get the current time
 {
     return m_rtc.now();
 }
 
-void DSRtc::set(DateTime &t)
+void DSRtc::set(DateTime &t)    // adjust the time
 {
     m_rtc.adjust(t);
 }
 
+/* overload of the set function to allow setting by unix timestamp */
 void DSRtc::set(uint32_t timeStamp)
 {
     m_rtc.adjust(DateTime(timeStamp));
 }
 
+/* print the date and time to the debug console */
 void DSRtc::print(const DateTime &t)
 {
     Serial.print(t.day(), DEC);
